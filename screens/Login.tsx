6a8d32a1d024ede2d08af013a2512cf38739cbd2
Umang Patel <umangpatel3105@gmail.com>
2023-10-27 12:53:03 +0530

    ui fixes

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View, Image, Button, Pressable } from "react-native";
 import React from "react";
 import Cover from "../assets/icons/Cover";
 import { Dimensions } from "react-native";
-import RightArrow from "../assets/icons/rightArrow";
+import RightArrow from "../assets/icons/RightArrow";
 const Login = () => {
   const width = Dimensions.get("screen").width;
   const height = Dimensions.get("screen").height;
@@ -15,28 +15,47 @@ const Login = () => {
           style={{ position: "absolute" }}
         />
       </View>
-      <Text style={{fontSize:32,color:"white",paddingVertical:4,justifyContent:"center",alignItems:"center",flex:0.7}} >Simpo</Text>
+      {/* <Text
+        style={{
+          fontSize: 32,
+          color: "white",
+          paddingVertical: 4,
+          justifyContent: "center",
+          alignItems: "center",
+          flex: 0.7,
+        }}
+      >
+        Simpo
+      </Text> */}
+      <View style={{ flex: 1 }}>
+        <Image
+          source={require("../assets/icons/pie.png")}
+          style={{ position: "relative",bottom:110,zIndex:18,width:'70%',height:'70%' }}
+        />
+      </View>
       <View
         style={{
           flex: 0.5,
           paddingHorizontal: 12,
           justifyContent: "space-around",
           // alignItems: "center",
-          borderWidth: 2,
+          // borderWidth: 2,
           borderColor: "red",
         }}
       >
-        <Text style={{fontSize:32,color:"white"}} >Wallet made Simple to use</Text>
+        <Text style={{ fontSize: 32, color: "white" }}>
+          Wallet made Simple to use
+        </Text>
         <Pressable
           style={{
             backgroundColor: "white",
-            flexDirection:"row",
+            flexDirection: "row",
             width: "100%",
             height: 48,
             borderRadius: 8,
-            justifyContent:"space-between",
-            alignItems:"center",
-            paddingHorizontal:24
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingHorizontal: 24,
           }}
         >
           <Text>Continue</Text>
